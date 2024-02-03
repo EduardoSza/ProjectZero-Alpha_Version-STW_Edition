3d445dc03fef6f9e39b81bcea5617d0fb0e114b5
ThiagoChaves517 <zuruarque@hotmail.com>
2024-02-03 14:34:33 -0400

    I haven't touch nothing kkkk

--- Assets/Scripts/Enviroment Controllers/BackgroundAttributes.cs
+++ Assets/Scripts/Enviroment Controllers/BackgroundAttributes.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class BackgroundAttributes : MonoBehaviour
 {
     [SerializeField]
+    private BoxCollider2D boxCollider;
+
+    [SerializeField]
+    private float width;
+    [SerializeField]
     private float speed = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        width = boxCollider.size.x; 
     }
 
     // Update is called once per frame
     void Update()
     {
         AllowMovingToLeft();
+
+        if (transform.position.x< -width)
+        {
+            Reposition();
+        }
     }
 
     // Permite movimentação constante para a esquerda:
@@ -33,5 +43,11 @@ public class BackgroundAttributes : MonoBehaviour
         Vector2 position = this.transform.position;
         position.x += speed * Time.deltaTime * GlobalVariables.globalSpeed;
         this.transform.position = position;
+    }
+
+    private void Reposition()
+    {
+        Vector2 vector = new Vector2 (width * 2f, 0);
+        transform.position = (Vector2) transform.position + vector;
     }
 }

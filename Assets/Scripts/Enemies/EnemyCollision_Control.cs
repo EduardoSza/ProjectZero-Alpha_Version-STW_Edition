ad9794ea98935766399bc8ead0854829408aa197
Henrique Oliveira <henrisoft3@gmail.com>
2024-04-09 21:38:34 -0300

    Update project

--- Assets/Scripts/Enemies/EnemyCollision_Control.cs
+++ Assets/Scripts/Enemies/EnemyCollision_Control.cs
@@ -4,33 +4,52 @@ using UnityEngine;
 
 public class EnemyCollision_Control : MonoBehaviour
 {
-    // Esta variável contêm o script que controla os atributos principais e o sistema de vida/morte do inimigo:
+
+    public Color damageColor;
+
+    [SerializeField]
+    private SpriteRenderer sprite;
+
+    // Esta variï¿½vel contï¿½m o script que controla os atributos principais e o sistema de vida/morte do inimigo:
     [SerializeField]
     private EnemyAttributes enemyAttributes;
-    // Esta variável contêm o script que permite ao inimigo se movimentar:
+    // Esta variï¿½vel contï¿½m o script que permite ao inimigo se movimentar:
     [SerializeField]
     private EnemyMovements enemyMovements;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Este trecho verificado se o game object encostando no inimigo contém o script SimpleShot_Control (ou seja, se é
+        // Este trecho verificado se o game object encostando no inimigo contï¿½m o script SimpleShot_Control (ou seja, se ï¿½
         // uma bala vinda do player). Se for, o inimigo leva o dano corresponente ao attackPower em PlayerAttributes:
         SimpleShot_Control simpleShot = collision.GetComponent<SimpleShot_Control>();
         SuperShot_Control superShot = collision.GetComponent<SuperShot_Control>();
+
+
         if (simpleShot != null)
         {
+            StartCoroutine(TakingDamage());
             enemyAttributes.LifePoints -= simpleShot.attackPower;
         }
         else if (superShot != null)
         {
+            StartCoroutine(TakingDamage());
             enemyAttributes.LifePoints -= superShot.attackPower;
         }
 
-        // Starting Point é um game object vazio, dotado de apenas um collisor em trigger.
-        // O uso desse if dependerá de com será programada a movimentação do inimigo em questão:
+        // Starting Point ï¿½ um game object vazio, dotado de apenas um collisor em trigger.
+        // O uso desse if dependerï¿½ de com serï¿½ programada a movimentaï¿½ï¿½o do inimigo em questï¿½o:
         if (collision.gameObject.CompareTag("StartingPoint"))
         {
             enemyMovements.StartFlag = true;
         }
     }
+
+    IEnumerator TakingDamage(){
+        sprite.color = damageColor;
+        yield return new WaitForSeconds(0.1f);
+        sprite.color = Color.white;
+    }
+
+
+    
 }
